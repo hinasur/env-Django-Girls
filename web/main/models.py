0009a92394e1e53043a6db84a137efb6d7c70f50
HiroshiN <hinasur252@gmail.com>
2020-11-08 14:47:55 +0900

    Created a login page with the Django-Girls tutorial.

--- web/main/models.py
+++ web/main/models.py
@@ -6,7 +6,22 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
 
 class Book(models.Model):
     title = models.TextField(blank=True, null=True)
