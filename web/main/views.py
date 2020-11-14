27a3c2711e0c639692227907eec0e348ee2dfb5f
HiroshiN <hinasur252@gmail.com>
2020-11-14 13:04:51 +0900

    Add a Django template to template.

--- web/main/views.py
+++ web/main/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def index(request):
+
+  posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
   # return HttpResponse(f'hello world')
-  return render(request, 'blog/index.html', {})
+  return render(request, 'blog/index.html', {'posts': posts})
